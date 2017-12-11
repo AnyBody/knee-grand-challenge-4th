e204ccd4ab2194dfd8088abff2b2de7440f5794f
mj <mj@anybodytech.com>
2017-12-11 16:48:56 +0100

    Update the model.

--- [Input]/Ligament/GCK4_Femur_Ligament_picked_points.pp
+++ [Input]/Ligament/GCK4_Femur_Ligament_picked_points.pp
@@ -6,10 +6,10 @@
   <DataFileName name="GCK4_Femur.stl"/>
   <templateName name=""/>
  </DocumentData>
- <point active="1" name="aALS" x="0.00502971" y="-0.0172651" z="-0.0410033"/>
- <point active="1" name="pALS" x="0.00625873" y="-0.015592" z="-0.0414518"/>
- <point active="1" name="aLCL" x="0.00908551" y="-0.0119496" z="-0.0419702"/>
- <point active="1" name="pLCL" x="0.0108738" y="-0.00887138" z="-0.042269"/>
+ <point active="1" name="aALS" x="0.011289" y="0.00164177" z="-0.0405309"/>
+ <point active="1" name="pALS" x="0.0108893" y="-0.000466555" z="-0.0411679"/>
+ <point active="1" name="aLCL" x="0.0112082" y="-0.00468456" z="-0.0419375"/>
+ <point active="1" name="pLCL" x="0.0116311" y="-0.00249683" z="-0.0412257"/>
  <point active="1" name="sLPFL" x="0.00680204" y="0.00120157" z="-0.0422114"/>
  <point active="1" name="mLPFL" x="0.00659284" y="-0.00274812" z="-0.0427914"/>
  <point active="1" name="iLPFL" x="0.00730188" y="-0.00556569" z="-0.0429244"/>
@@ -22,10 +22,17 @@
  <point active="1" name="pMCL" x="0.00979632" y="-0.00507421" z="0.042633"/>
  <point active="1" name="aCM" x="0.00785659" y="-0.0135558" z="0.0420058"/>
  <point active="1" name="pCM" x="0.0135791" y="-0.0142812" z="0.0412461"/>
+ <!--
+ <point active="1" name="mMcap" x="0.014857" y="0.00805996" z="0.031014"/>
  <point active="1" name="lMcap" x="0.0144419" y="0.0079701" z="0.014761"/>
  <point active="1" name="mLcap" x="0.0171896" y="0.00642519" z="-0.0173145"/>
  <point active="1" name="lLcap" x="0.0186229" y="0.00699682" z="-0.0308602"/>
+ -->
+ <point active="1" name="mMcap" x="0.014857" y="0.01005996" z="0.031014"/>
+ <point active="1" name="lMcap" x="0.0144419" y="0.0099701" z="0.014761"/>
+ <point active="1" name="mLcap" x="0.0171896" y="0.00955" z="-0.0173145"/>
+ <point active="1" name="lLcap" x="0.0186229" y="0.00999682" z="-0.0308602"/> 
  <point active="1" name="aPCL" x="0.0088112" y="-0.0163553" z="0.00813052"/>
  <point active="1" name="pPCL" x="0.0154319" y="-0.0184329" z="0.0128823"/>
- <point active="1" name="mMcap" x="0.014857" y="0.00805996" z="0.031014"/>
+
 </PickedPoints>
--- [Input]/Ligament/GCK4_Tibia_Ligament_picked_points.pp
+++ [Input]/Ligament/GCK4_Tibia_Ligament_picked_points.pp
@@ -1,22 +1,22 @@
 <!DOCTYPE PickedPoints>
 <PickedPoints>
  <DocumentData>
-  <DateTime date="2017-10-02" time="14:33:41"/>
+  <DateTime time="08:51:23" date="2017-12-07"/>
   <User name="mj"/>
   <DataFileName name="GCK4_Tibia.stl"/>
   <templateName name=""/>
  </DocumentData>
- <point z="-0.0378053" x="-0.00464375" name="aALS" y="-0.0192675" active="1"/>
- <point z="-0.0378293" x="-8.93789e-5" name="pALS" y="-0.0196218" active="1"/>
- <point z="0.0241882" x="-0.0043997" name="aMCL" y="-0.0505355" active="1"/>
- <point z="0.0238012" x="-0.000850892" name="cMCL" y="-0.0514943" active="1"/>
- <point z="-0.0320562" x="0.020858" name="lLcap" y="-0.0111704" active="1"/>
- <point z="-0.00944432" x="0.0229502" name="mLcap" y="-0.0112664" active="1"/>
- <point z="0.00202225" x="0.02249" name="pPCL" y="-0.0106882" active="1"/>
- <point z="-0.00157739" x="0.0185957" name="aPCL" y="-0.010394" active="1"/>
- <point z="0.0410914" x="0.000845775" name="aCM" y="-0.0120243" active="1"/>
- <point z="0.040148" x="0.00709624" name="pCM" y="-0.0118619" active="1"/>
- <point z="0.0360003" x="0.00561727" name="pMCL" y="-0.0227132" active="1"/>
- <point z="0.0306316" x="0.0213735" name="mMcap" y="-0.0117325" active="1"/>
- <point z="0.0106129" x="0.0254384" name="lMcap" y="-0.0121408" active="1"/>
+ <point y="-0.0203032" name="aALS" active="1" x="-0.011975" z="-0.0362425"/>
+ <point y="-0.0202552" name="pALS" active="1" x="-0.00777133" z="-0.0371993"/>
+ <point y="-0.0505355" name="aMCL" active="1" x="-0.0043997" z="0.0241882"/>
+ <point y="-0.0514943" name="cMCL" active="1" x="-0.000850892" z="0.0238012"/>
+ <point y="-0.0111704" name="lLcap" active="1" x="0.020858" z="-0.0320562"/>
+ <point y="-0.0112664" name="mLcap" active="1" x="0.0229502" z="-0.00944432"/>
+ <point y="-0.0106882" name="pPCL" active="1" x="0.02249" z="0.00202225"/>
+ <point y="-0.010394" name="aPCL" active="1" x="0.0185957" z="-0.00157739"/>
+ <point y="-0.0120243" name="aCM" active="1" x="0.000845775" z="0.0410914"/>
+ <point y="-0.0118619" name="pCM" active="1" x="0.00709624" z="0.040148"/>
+ <point y="-0.0227132" name="pMCL" active="1" x="0.00561727" z="0.0360003"/>
+ <point y="-0.0117325" name="mMcap" active="1" x="0.0213735" z="0.0306316"/>
+ <point y="-0.0121408" name="lMcap" active="1" x="0.0254384" z="0.0106129"/>
 </PickedPoints>
