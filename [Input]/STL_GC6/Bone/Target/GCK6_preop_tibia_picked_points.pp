e60bc2c45398edc99bebd2b83caa5ef32dfebb47
Maria Paz <maria.paz@materialise.be>
2018-03-21 09:55:10 +0100

    GC6 STLs and picked points for target bone

---
+++ [Input]/STL_GC6/Bone/Target/GCK6_preop_tibia_picked_points.pp
@@ -0,0 +1,29 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime time="17:53:24" date="2017-08-09"/>
+  <User name="Administrator"/>
+  <DataFileName name=""/>
+  <templateName name=""/>
+ </DocumentData>
+ <point x="-0.0300061" y="0.0259009" z="-0.3994069" active="1" name="0"/>
+ <point x="-0.0118900" y="0.0425821" z="-0.4007433" active="1" name="1"/>
+ <point x="0.0116855" y="0.0024077" z="-0.4249634" active="1" name="2"/>
+ <point x="0.0212831" y="0.0123545" z="-0.4208004" active="1" name="3"/>
+ <point x="0.0170860" y="0.0196691" z="-0.4218145" active="1" name="4"/>
+ <point x="-0.0081843" y="0.0432082" z="-0.4125334" active="1" name="5"/>
+ <point x="-0.0023047" y="0.0183072" z="-0.4113907" active="1" name="6"/>
+ <point x="-0.0115203" y="-0.0010375" z="-0.2523171" active="1" name="7"/>
+ <point x="-0.0076656" y="-0.0094692" z="-0.1793951" active="1" name="8"/>
+ <point x="-0.0096392" y="-0.0202608" z="-0.0706803" active="1" name="9"/>
+ <point x="0.0073071" y="0.0186520" z="-0.0217498" active="1" name="10"/>
+ <point x="-0.0014974" y="0.0210272" z="-0.0210848" active="1" name="11"/>
+ <point x="-0.0237085" y="0.0204609" z="-0.0347042" active="1" name="12"/>
+ <point x="0.0260888" y="0.0133836" z="-0.0326035" active="1" name="13"/>
+ <point x="-0.0461519" y="0.0209882" z="-0.0390661" active="1" name="14"/>
+ <point x="0.0427110" y="0.0088415" z="-0.0368052" active="1" name="15"/>
+ <point x="0.0123675" y="0.0416698" z="-0.0435230" active="1" name="16"/>
+ <point x="-0.0061558" y="0.0465424" z="-0.0403184" active="1" name="17"/>
+ <point x="-0.0055736" y="0.0002146" z="-0.0310868" active="1" name="18"/>
+ <point x="0.0097018" y="0.0320697" z="-0.0349108" active="1" name="19"/>
+</PickedPoints>
