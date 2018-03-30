0f97de199ab1ee3d30fb93ef70944a3b55ebd0e1
Laura Bartsoen <laura.bartsoen@hotmail.com>
2018-03-30 14:17:54 +0200

    Added hip and ankle joints

---
+++ [Input]/STL_GC6 - Copy/Bone/Target/GCK6_preop_femur_picked_points.pp
@@ -0,0 +1,29 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DateTime time="17:23:08" date="2017-08-09"/>
+  <User name="Administrator"/>
+  <DataFileName name=""/>
+  <templateName name=""/>
+ </DocumentData>
+ <point x="-0.0329613" y="0.0183502" z="-0.0267070" active="1" name="0"/>
+ <point x="0.0208703" y="0.0161008" z="-0.0276544" active="1" name="1"/>
+ <point x="-0.0526855" y="0.0138764" z="0.0016510" active="1" name="2"/>
+ <point x="0.0367079" y="-0.0008357" z="0.0028562" active="1" name="3"/>
+ <point x="-0.0315546" y="-0.0344512" z="0.0150569" active="1" name="4"/>
+ <point x="0.0058143" y="-0.0310425" z="-0.0060670" active="1" name="5"/>
+ <point x="-0.0316421" y="0.0396683" z="-0.0029377" active="1" name="6"/>
+ <point x="0.0226080" y="0.0355049" z="-0.0010073" active="1" name="7"/>
+ <point x="-0.0422281" y="-0.0253364" z="0.2094135" active="1" name="8"/>
+ <point x="-0.0383880" y="0.0048390" z="0.2067190" active="1" name="9"/>
+ <point x="-0.0393223" y="-0.0126769" z="0.3601577" active="1" name="10"/>
+ <point x="-0.0190072" y="0.0118596" z="0.3595576" active="1" name="11"/>
+ <point x="-0.0418854" y="0.0330681" z="0.3879869" active="1" name="12"/>
+ <point x="-0.0683296" y="0.0331389" z="0.3854502" active="1" name="13"/>
+ <point x="-0.0811791" y="0.0203418" z="0.3866687" active="1" name="14"/>
+ <point x="-0.0771781" y="0.0054464" z="0.379211" active="1" name="15"/>
+ <point x="-0.0464201" y="0.0265184" z="0.4236650" active="1" name="16"/>
+ <point x="-0.0725349" y="0.0001747" z="0.4120484" active="1" name="17"/>
+ <point x="-0.0577818" y="-0.0105683" z="0.3898870" active="1" name="18"/>
+ <point x="-0.0514377" y="0.0225729" z="0.4073853" active="1" name="19"/>
+</PickedPoints>
