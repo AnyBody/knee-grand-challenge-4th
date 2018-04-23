9eebf46db67c79043a393b2066eb87f6cafb5425
Maria Paz <maria.paz@materialise.be>
2018-04-23 11:50:13 +0200

    adapt GC6 coordinate system to GC4. New stls and pp files

--- [Input]/Ligament_GC6/GCK6_Femur_Ligament_picked_points.pp
+++ [Input]/Ligament_GC6/GCK6_Femur_Ligament_picked_points.pp
@@ -3,26 +3,26 @@
  <DocumentData>
   <DataFileName name="D:\AnyBodyModels\git_gc_4\[Input]\STL_GC6\Bone\Target\GCK6_postop_femur.stl"/>
  </DocumentData>
- <point x="-0.0477892" y="0.0212444" z="0.0087375" name="aALS" active="1"/>
- <point x="-0.0481969" y="0.0220375" z="0.0058769" name="pALS" active="1"/>
- <point x="-0.0499337" y="0.0216142" z="0.0004496" name="aLCL" active="1"/>
- <point x="-0.0492507" y="0.0221603" z="0.0021965" name="pLCL" active="1"/>
- <point x="-0.0492459" y="0.0173892" z="0.0106881" name="sLPFL" active="1"/>
- <point x="-0.0514921" y="0.0173298" z="0.0032582" name="mLPFL" active="1"/>
- <point x="-0.051708" y="0.017001" z="-0.0002093" name="iLPFL" active="1"/>
- <point x="-0.0484985" y="0.0162646" z="-0.0102728" name="PFL" active="1"/>
- <point x="0.0310343" y="-0.0019953" z="0.0139798" name="sMPFL" active="1"/>
- <point x="0.0333995" y="-0.0030628" z="0.0098808" name="mMPFL" active="1"/>
- <point x="0.0343065" y="-0.0034122" z="0.0069552" name="iMPFL" active="1"/>
- <point x="0.036698" y="0.0025125" z="0.0069095" name="aMCL" active="1"/>
- <point x="0.0370482" y="0.0065288" z="0.0065153" name="cMCL" active="1"/>
- <point x="0.0370368" y="0.0096172" z="0.0060661" name="pMCL" active="1"/>
- <point x="0.0343238" y="0.0086114" z="-0.0058734" name="aCM" active="1"/>
- <point x="0.0347244" y="0.0136185" z="-0.0053001" name="pCM" active="1"/>
- <point x="0.0242167" y="0.016392" z="0.0213364" name="mMcap" active="1"/>
- <point x="0.0098879" y="0.0176755" z="0.0226685" name="lMcap" active="1"/>
- <point x="-0.0214004" y="0.0210855" z="0.0189538" name="mLcap" active="1"/>
- <point x="-0.0393877" y="0.0222009" z="0.0190091" name="lLcap" active="1"/>
- <point x="0.0005307" y="0.0122913" z="-0.0057039" name="aPCL" active="1"/>
- <point x="0.006017" y="0.0202318" z="-0.0116297" name="pPCL" active="1"/>
+ <point z="-0.0477892" x="0.0212444" y="0.0087375" name="aALS" active="1"/>
+ <point z="-0.0481969" x="0.0220375" y="0.0058769" name="pALS" active="1"/>
+ <point z="-0.0499337" x="0.0216142" y="0.0004496" name="aLCL" active="1"/>
+ <point z="-0.0492507" x="0.0221603" y="0.0021965" name="pLCL" active="1"/>
+ <point z="-0.0492459" x="0.0173892" y="0.0106881" name="sLPFL" active="1"/>
+ <point z="-0.0514921" x="0.0173298" y="0.0032582" name="mLPFL" active="1"/>
+ <point z="-0.051708" x="0.017001" y="-0.0002093" name="iLPFL" active="1"/>
+ <point z="-0.0484985" x="0.0162646" y="-0.0102728" name="PFL" active="1"/>
+ <point z="0.0310343" x="-0.0019953" y="0.0139798" name="sMPFL" active="1"/>
+ <point z="0.0333995" x="-0.0030628" y="0.0098808" name="mMPFL" active="1"/>
+ <point z="0.0343065" x="-0.0034122" y="0.0069552" name="iMPFL" active="1"/>
+ <point z="0.036698" x="0.0025125" y="0.0069095" name="aMCL" active="1"/>
+ <point z="0.0370482" x="0.0065288" y="0.0065153" name="cMCL" active="1"/>
+ <point z="0.0370368" x="0.0096172" y="0.0060661" name="pMCL" active="1"/>
+ <point z="0.0343238" x="0.0086114" y="-0.0058734" name="aCM" active="1"/>
+ <point z="0.0347244" x="0.0136185" y="-0.0053001" name="pCM" active="1"/>
+ <point z="0.0242167" x="0.016392" y="0.0213364" name="mMcap" active="1"/>
+ <point z="0.0098879" x="0.0176755" y="0.0226685" name="lMcap" active="1"/>
+ <point z="-0.0214004" x="0.0210855" y="0.0189538" name="mLcap" active="1"/>
+ <point z="-0.0393877" x="0.0222009" y="0.0190091" name="lLcap" active="1"/>
+ <point z="0.0005307" x="0.0122913" y="-0.0057039" name="aPCL" active="1"/>
+ <point z="0.006017" x="0.0202318" y="-0.0116297" name="pPCL" active="1"/>
 </PickedPoints>
