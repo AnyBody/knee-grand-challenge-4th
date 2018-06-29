05d7847342433fe94733f8cde3415cef5622017c
Maria Paz <maria.paz@materialise.be>
2018-06-29 11:29:30 +0200

    picked points for knee axes

---
+++ [Input]/STL_GC6/Implant/femoral_component_knee_axes_picked_points.pp
@@ -0,0 +1,56 @@
+<!DOCTYPE PickedPoints>
+<PickedPoints>
+ <DocumentData>
+  <DataFileName name="D:\AnyBodyModels\git_gc_4\[Input]\STL_GC6\Implant\femoral_component.stl"/>
+ </DocumentData>
+ <point x="-1.16e-05" y="-2.89e-05" z="1.08e-05" name="0" active="1"/>
+ <point x="-1.03e-05" y="-2.95e-05" z="1.65e-05" name="1" active="1"/>
+ <point x="-9.9e-06" y="-3e-05" z="-2.53e-05" name="2" active="1"/>
+ <point x="-8.6e-06" y="-3.08e-05" z="-3.08e-05" name="3" active="1"/>
+ <point x="3.38e-05" y="-2.48e-05" z="-3.59e-05" name="4" active="1"/>
+ <point x="3.29e-05" y="-2.44e-05" z="-2.35e-05" name="5" active="1"/>
+ <point x="3.32e-05" y="-2.49e-05" z="-3.01e-05" name="6" active="1"/>
+ <point x="2.96e-05" y="-2.38e-05" z="1.23e-05" name="7" active="1"/>
+ <point x="2.88e-05" y="-2.32e-05" z="2.69e-05" name="8" active="1"/>
+ <point x="2.97e-05" y="-2.29e-05" z="2.07e-05" name="9" active="1"/>
+ <point x="2.91e-05" y="-2.93e-05" z="-3.45e-05" name="10" active="1"/>
+ <point x="2.48e-05" y="-3.22e-05" z="-3.68e-05" name="11" active="1"/>
+ <point x="1.88e-05" y="-3.42e-05" z="-3.43e-05" name="12" active="1"/>
+ <point x="1.17e-05" y="-3.52e-05" z="-3.35e-05" name="13" active="1"/>
+ <point x="2e-06" y="-3.46e-05" z="-3.53e-05" name="14" active="1"/>
+ <point x="-1.5e-06" y="-3.36e-05" z="-3.1e-05" name="15" active="1"/>
+ <point x="-2e-07" y="-3.37e-05" z="-2.39e-05" name="16" active="1"/>
+ <point x="2.71e-05" y="-2.95e-05" z="-2.23e-05" name="17" active="1"/>
+ <point x="1.96e-05" y="-3.32e-05" z="-2.3e-05" name="18" active="1"/>
+ <point x="1.07e-05" y="-3.48e-05" z="-2.47e-05" name="19" active="1"/>
+ <point x="7e-06" y="-3.5e-05" z="-2.88e-05" name="20" active="1"/>
+ <point x="1.89e-05" y="-3.38e-05" z="-2.91e-05" name="21" active="1"/>
+ <point x="2.39e-05" y="-3.19e-05" z="-2.76e-05" name="22" active="1"/>
+ <point x="-1.2e-06" y="-3.26e-05" z="1.13e-05" name="23" active="1"/>
+ <point x="9e-06" y="-3.32e-05" z="1.22e-05" name="24" active="1"/>
+ <point x="1.86e-05" y="-3.12e-05" z="1.16e-05" name="25" active="1"/>
+ <point x="2.59e-05" y="-2.72e-05" z="1.15e-05" name="26" active="1"/>
+ <point x="2.45e-05" y="-2.71e-05" z="2.75e-05" name="27" active="1"/>
+ <point x="1.86e-05" y="-3.04e-05" z="2.76e-05" name="28" active="1"/>
+ <point x="1.29e-05" y="-3.2e-05" z="2.66e-05" name="29" active="1"/>
+ <point x="6.1e-06" y="-3.28e-05" z="2.47e-05" name="30" active="1"/>
+ <point x="0.0" y="-3.25e-05" z="2.09e-05" name="31" active="1"/>
+ <point x="2e-06" y="-3.29e-05" z="1.68e-05" name="32" active="1"/>
+ <point x="9e-06" y="-3.3e-05" z="1.78e-05" name="33" active="1"/>
+ <point x="1.65e-05" y="-3.16e-05" z="1.97e-05" name="34" active="1"/>
+ <point x="2.39e-05" y="-2.84e-05" z="1.71e-05" name="35" active="1"/>
+ <point x="2.4e-05" y="-2.8e-05" z="2.1e-05" name="36" active="1"/>
+ <point x="-6.4e-06" y="-3.1e-05" z="1.84e-05" name="37" active="1"/>
+ <point x="1.46e-05" y="-3.46e-05" z="-2.82e-05" name="38" active="1"/>
+ <point x="5.2e-06" y="-3.43e-05" z="-2.2e-05" name="39" active="1"/>
+ <point x="-6.2e-06" y="-3.12e-05" z="1.42e-05" name="40" active="1"/>
+ <point x="1.32e-05" y="-3.25e-05" z="1.61e-05" name="41" active="1"/>
+ <point x="2.09e-05" y="-2.95e-05" z="2.4e-05" name="42" active="1"/>
+ <point x="2.67e-05" y="-2.62e-05" z="1.79e-05" name="43" active="1"/>
+ <point x="2.74e-05" y="-2.99e-05" z="-2.81e-05" name="44" active="1"/>
+ <point x="2.36e-05" y="-3.25e-05" z="-3.3e-05" name="45" active="1"/>
+ <point x="6.1e-06" y="-3.52e-05" z="-3.55e-05" name="46" active="1"/>
+ <point x="1.2e-05" y="-3.25e-05" z="2.11e-05" name="47" active="1"/>
+ <point x="4.3e-06" y="-3.32e-05" z="1.04e-05" name="48" active="1"/>
+ <point x="1.41e-05" y="-3.43e-05" z="-2.27e-05" name="49" active="1"/>
+</PickedPoints>
